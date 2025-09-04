e01d034748493d6eb905f3c81b333ad661f4d43e
zoasr <44146518+ZOASR@users.noreply.github.com>
2025-09-04 18:45:24 +0300

    refactor(ui): Improve error display in ErrorComponent

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -4,20 +4,17 @@ import { Button } from "./ui/button";
 export default function ErrorComponent({ error }) {
 	return (
 		<article className="w-full h-dvh grid place-items-center">
-			<section className="text-center space-y-8">
-				<h1 className="text-9xl text-foreground font-bold">
+			<section className="text-center space-y-8 bg-destructive/20 p-4 rounded-md border-2 border-destructive/30">
+				<h1 className="text-lg text-destructive font-bold text-start  overflow-x-scroll">
+					An Error happened : with the following message:
+				</h1>
+				<pre className="bg-accent/20 rounded-sm p-2 border border-accent text-start">
 					{error instanceof Error
 						? error.message
 						: error.data?.httpStatus || 500}
-				</h1>
-				<p className="text-2xl font-light">
-					An error has occurred with the message:{" "}
-					{error instanceof Error
-						? error.message
-						: error.data?.code || "Unknown Error"}
-				</p>
+				</pre>
 				<Link to="..">
-					<Button>Go Back</Button>
+					<Button variant="destructive">Go Back</Button>
 				</Link>
 			</section>
 		</article>
